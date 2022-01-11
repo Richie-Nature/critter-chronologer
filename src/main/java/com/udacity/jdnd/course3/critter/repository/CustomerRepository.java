8b4ebd67b398e4c297cc991c0be4c6a7318163d5
Imoh Obot <obotimoh20@gmail.com>
2022-01-11 12:31:53 +0100

    create data access layer using spring data repositories

---
+++ src/main/java/com/udacity/jdnd/course3/critter/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.udacity.jdnd.course3.critter.repository;
+
+import com.udacity.jdnd.course3.critter.pet.Pet;
+import com.udacity.jdnd.course3.critter.user.customer.Customer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    Customer findByPetDistinctContaining(Pet pet);
+}
---
+++ src/main/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepository.java
@@ -0,0 +1,16 @@
+package com.udacity.jdnd.course3.critter.repository;
+
+import com.udacity.jdnd.course3.critter.pet.Pet;
+import com.udacity.jdnd.course3.critter.schedule.Schedule;
+import com.udacity.jdnd.course3.critter.user.customer.Customer;
+import com.udacity.jdnd.course3.critter.user.employee.Employee;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ScheduleRepository extends CrudRepository<Schedule, Long> {
+
+    List<Schedule> findAllByEmployeeContaining(Employee employee);
+    List<Schedule> findAllByPetContaining(Pet pet);
+    List<Schedule> findAllByCustomerContaining(Customer customer);
+}
